ab67e1569b8647715fdcacc1d129dcf97a3fa9b9
Tymofiy Bun <1931850@champlaincollege.qc.ca>
2021-12-06 00:25:31 -0500

    accually worked on the nav

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,53 +1,55 @@
 import axios from 'axios'
 import { useRouter } from 'next/router'
-import {url} from '../constants/urls'
-
+import { url } from '../constants/urls'
 const getUrl = url
-const logoutUrl = getUrl+"/logout"
+const logoutUrl = getUrl + "/logout"
 
-export default function Navbar(props)
-{
+export default function Navbar(props) {
   const router = useRouter()
 
-  async function logout(){
-    try{
-      let response = await axios.post(logoutUrl, {}, {withCredentials:true})
+  async function logout() {
+    try {
+      let response = await axios.post(logoutUrl, {}, { withCredentials: true })
       router.push('/login')
-    }catch(e){
+    } catch (e) {
       console.log(e)
     }
   }
-    return(
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-        <a className="navbar-brand" href="#">Navbar</a>
-        <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-          <span className="navbar-toggler-icon"></span>
-        </button>
-        <div className="collapse navbar-collapse" id="navbarNavDropdown">
-          <ul className="navbar-nav">
-            <li className="nav-item active">
-              <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Features</a>
-            </li>
-            <li className="nav-item">
-              <a className="nav-link" href="#">Pricing</a>
-            </li>
-            <li className="nav-item dropdown">
-              <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                Dropdown link
-              </a>
-              <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                <a className="dropdown-item" href="#">Action</a>
-                <a className="dropdown-item" href="#">Another action</a>
-                <a className="dropdown-item" href="#">Something else here</a>
-              </div>
-            </li>
-            
-            <button className="btn btn-outline-danger my-2 my-sm-0" onClick={logout}>Logout</button>
-          </ul>
-        </div>
-      </nav>
+  return (
+    <nav className="navbar navbar-expand-lg navbar-light bg-light">
+      <a className="navbar-brand" href="#">
+        <img src='/images/PiLogo.png' width="65" height="50" className="d-inline-block align-top" alt="" />
+        PI Temperature
+      </a>
+      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+        <span className="navbar-toggler-icon"></span>
+      </button>
+      <div className="collapse navbar-collapse" id="navbarNavDropdown">
+        <ul className="navbar-nav">
+          <li className="nav-item active">
+            <a className="nav-link" href="#">Home <span className="sr-only">(current)</span></a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Features</a>
+          </li>
+          <li className="nav-item">
+            <a className="nav-link" href="#">Pricing</a>
+          </li>
+          <li className="nav-item dropdown">
+            <a className="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+              Dropdown link
+            </a>
+            <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+              <a className="dropdown-item" href="#">Action</a>
+              <a className="dropdown-item" href="#">Another action</a>
+              <a className="dropdown-item" href="#">Something else here</a>
+            </div>
+          </li>
 
-    )}
+          <button className="btn btn-outline-danger my-2 my-sm-0" onClick={logout}>Logout</button>
+        </ul>
+      </div>
+    </nav>
+
+  )
+}
