c8eeb4bc9e91c4a285728a6aa529b6004e38d816
ArianneBonneau <arianne.bonneau@hotmail.com>
2021-12-05 19:34:28 -0500

    ui for login page

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,9 @@
 import '../styles/globals.css'
 // import '../styles/navStyle.css'
 import 'bootstrap/dist/css/bootstrap.css'
+import React from 'react';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+
 function MyApp({ Component, pageProps })
 {
 
