c17840151214c43b31d608d76cfa52da96da3836
Christian Chitanu <christian.chitanu@hotmail.com>
2021-12-05 20:05:22 -0500

    fixed build bugs

--- pages/_app.js
+++ pages/_app.js
@@ -1,8 +1,6 @@
 import '../styles/globals.css'
 // import '../styles/navStyle.css'
 import 'bootstrap/dist/css/bootstrap.css'
-import React from 'react';
-import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
 function MyApp({ Component, pageProps })
 {
