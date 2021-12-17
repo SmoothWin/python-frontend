20aec2db4801aa60100b22de06ff833a98ff9c9e
Christian Chitanu <christian.chitanu@hotmail.com>
2021-12-17 17:13:59 -0500

    changed a few things

--- constants/urls.js
+++ constants/urls.js
@@ -1,9 +1,3 @@
 export const url = "https://pythontemperaturetracker.herokuapp.com" //for production
-export const humidityUrl = "https://pythontemperaturetracker.herokuapp.com/humitidy"
-export const temperatureUrl = "https://pythontemperaturetracker.herokuapp.com/temperature"
-export const statusUrl = "https://pythontemperaturetracker.herokuapp.com/status"
 
 // export const url = "http://localhost:5000" //for development
-// export const humidityUrl = "http://localhost:5000/humitidy"
-// export const temperatureUrl = "http://localhost:5000/temperature"
-// export const statusUrl = "http://localhost:5000/status"
